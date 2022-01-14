21df728e403752cf106f4a9dc341e0099648cf33
timmartinp <92250872+timmartinp@users.noreply.github.com>
2022-01-14 10:32:38 +0100

    update index.js

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ import fetch from "node-fetch";
 var spotifyToken = ''
 const tmToken = '7elxdku9GGG5k8j0Xm8KWdANDgecHMV0'
 var countryCode = 'DE'
+const wmApiKey = 'c2eb12c79f9416c24f32b54fa43cdada'
+const artist = 'Metallica'
+const radius = '100'
+const pagesize = '50'
+var location = await getLocation();
 
 
 var client_id = '9f5344d0b735484b803e2515e98d68bc';
 var client_secret = 'f20f8ac67914469295c296aa9fc6b224';
 
 async function getToken() {
-  
-  
 
   const response = await fetch('https://accounts.spotify.com/api/token', {
   method: 'POST',
@@ -29,7 +32,8 @@ async function getToken() {
 
 async function getArtistsGenres(_token) {
 
-  const response = await fetch(`https://api.spotify.com/v1/search?q=Metallica&type=artist&limit=1`, {
+  
+  const response = await fetch(`https://api.spotify.com/v1/search?q=${artist}&type=artist&limit=1`, {
     method: 'GET',
     headers: {'Authorization': 'Bearer ' + _token}
   });
@@ -49,6 +53,7 @@ async function getGenres(_token) {
 
 
 function getGenreID(_artistsGenres, _tmGenres) {
+
   var foundSubGenre = new Array;
   var foundGenre = new Array;
 
@@ -80,15 +85,57 @@ function getGenreID(_artistsGenres, _tmGenres) {
 }
 
 
+async function getLocation() {
 
-async function eventSearch(genreIds, _token) {
-  const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events?apikey=${_token}&locale=*&countryCode=${countryCode}&subGenreId=${genreIds.subGenres}`);
+  const city = 'Koeln'
+  const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${wmApiKey}`)
 
   const data = await response.json();
-  return data;
+
+  const latlon = {
+    lat: data[0].lat,
+    lon: data[0].lon
+  }
+  return latlon;
+}
+
+async function getArtistEvents(_token) {
+  const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events?apikey=${_token}&locale=*&latlong=${location.lat},${location.lon}&radius=${radius}&size=${pagesize}&keyword=${artist}`);
+
+  const data = await response.json();
+  return data._embedded.events;
 }
 
 
+async function eventSearch(genreIds, _token) {
+
+  
+  var genreEvents = new Array;
+  var subGenreEvents = new Array;
+  
+  for (let i = 0; i < genreIds.genres.length; i++) {
+    const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events?apikey=${_token}&locale=*&latlong=${location.lat},${location.lon}&radius=${radius}&size=${pagesize}&genreId=${genreIds.genres[i]}`);
+    var temp = await response.json();
+    if(temp._embedded != undefined) {
+      genreEvents.push(temp._embedded.events);
+    }
+  };
+
+  for (let i = 0; i < genreIds.subGenres.length; i++) {
+    const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events?apikey=${_token}&locale=*&latlong=${location.lat},${location.lon}&radius=${radius}&size=${pagesize}&subGenreId=${genreIds.subGenres[i]}`);
+    var temp = await response.json();
+    if(temp._embedded != undefined) {
+      subGenreEvents.push(temp._embedded.events);
+    }
+  };
+
+  const events = {
+    genreEvents: genreEvents,
+    subGenreEvents: subGenreEvents
+  }
+
+  return events;
+}
 
 
 
@@ -96,5 +143,9 @@ spotifyToken = await getToken();
 var artistsGenres = await getArtistsGenres(spotifyToken);
 var tmGenres = await getGenres(tmToken);
 var genreIds = getGenreID(artistsGenres, tmGenres);
+
+var artistEvents = await getArtistEvents(tmToken);
 var events = await eventSearch(genreIds, tmToken);
-console.log(events);
+console.log(artistEvents);
+console.log(events.genreEvents);
+console.log(events.subGenreEvents);
